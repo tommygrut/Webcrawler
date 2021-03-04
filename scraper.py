32e4ff0cbce57c84aa1df9f4d0451c69595d304e
tommygrut <34200893+tommygrut@users.noreply.github.com>
2021-03-04 17:14:45 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -22,17 +22,18 @@ for article in soup.find_all('article'):
     try:
         vid_src = article.find('iframe', class_='youtube-player')['src']
 
-        vid_id = vid_src.split('/')[4]
-        vid_id = vid_id.split('?')[0]
+        vid_id = vid_src.split('/')[-1]
+        vid_id = vid_id.split('?')[0]   #figure out what this is doing, what are the values
 
         yt_link = f'https://youtube.com/watch?v={vid_id}'
     except Exception as e:
         yt_link = None
 
-    print(yt_link)
+    print(yt_link, '\n')
 
-    print()
+   
 
     csv_writer.writerow([headline, summary, yt_link])
 
 csv_file.close()
+
